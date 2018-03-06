59140c7a991b5f1b8b6a0e4a38ddbac57e35343c
Joan Arcas <drumsjuanma@gmail.com>
2018-03-06 13:29:10 +0100

    Update startChannel.sh

--- first-network/scripts/startChannel.sh
+++ first-network/scripts/startChannel.sh
@@ -19,12 +19,14 @@ peer chaincode install -n test -v 1.0 -p github.com/hyperledger/fabric/examples/
 
 echo -e '\n\n\e[92m//////// --- Instanciando Chaincode --- ////////\e[39m'
 peer chaincode instantiate -o orderer.alastria.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/alastria.com/orderers/orderer.alastria.com/msp/tlscacerts/tlsca.alastria.com-cert.pem -C $CHANNEL_NAME -n test -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('CaixabankMSP.member','AlastriaMSP.member')"
+sleep 5
 
 echo -e '\n\n\e[92m//////// --- Query Chaincode --- ////////\e[39m'
 peer chaincode query -C $CHANNEL_NAME -n test -c '{"Args":["query","a"]}'
 
 echo -e '\n\n\e[92m//////// --- Invoke Chaincode --- ////////\e[39m'
 peer chaincode invoke -o orderer.alastria.com:7050  --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/alastria.com/orderers/orderer.alastria.com/msp/tlscacerts/tlsca.alastria.com-cert.pem  -C $CHANNEL_NAME -n test -c '{"Args":["invoke","a","b","10"]}'
+sleep 5
 
 echo -e '\n\n\e[92m//////// --- Query Chaincode --- ////////\e[39m'
 peer chaincode query -C $CHANNEL_NAME -n test -c '{"Args":["query","a"]}'
