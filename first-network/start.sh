0f82e270c4acc5f1f7dfb7b24706ea893aa21034
Joan Arcas <drumsjuanma@gmail.com>
2018-03-06 13:13:37 +0100

    Create start.sh

---
+++ first-network/start.sh
@@ -0,0 +1,7 @@
+echo -e '\e[42m//////// --- Iniciando Red AlastriaNet --- ////////\e[49m'
+
+export CHANNEL_NAME=mychannel
+IMAGETAG="latest"
+IMAGE_TAG=$IMAGETAG CHANNEL_NAME=$CHANNEL_NAME docker-compose -f docker-compose-cli.yaml up
+
+echo -e '\e[92m//////// --- Exito --- ////////\e[39m'
